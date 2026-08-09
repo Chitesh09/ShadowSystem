f42120137248f2c23ef7fa44295877c62a9efbf4
Rajapu Chitesh <chiteshrajapu09@gmail.com>
2026-08-09 23:44:55 +0530

    fix(layout): remove duplicate AppLayout wrapper from RootLayout to ensure exactly one single top header and hamburger menu

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import "./globals.css";
 import { ThemeProvider } from "../context/ThemeContext";
 import { SystemProvider } from "../context/SystemContext";
-import AppLayout from "../components/layout/AppLayout";
 
 export const metadata = {
   title: "Shadow System - Life RPG Operating System",
@@ -21,9 +20,7 @@ export default function RootLayout({ children }) {
       <body>
         <ThemeProvider>
           <SystemProvider>
-            <AppLayout>
-              {children}
-            </AppLayout>
+            {children}
           </SystemProvider>
         </ThemeProvider>
       </body>
